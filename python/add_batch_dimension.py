336630d25a023d0665e87a0336c025106ac085dc
Luc <lucdael1999@gmail.com>
2025-01-30 11:33:47 +0100

    added function description

--- python/add_batch_dimension.py
+++ python/add_batch_dimension.py
@@ -1,13 +1,6 @@
 import tensorflow as tf
 
+#this function will add a batch dimension to a tensor. 
 def add_batch_dimension(tensor):
-    """
-    Adds a batch dimension to the input tensor.
 
-    Args:
-        tensor (tf.Tensor): The input tensor to which the batch dimension will be added.
-
-    Returns:
-        tf.Tensor: The input tensor with an added batch dimension.
-    """
     return tf.expand_dims(tensor, axis=0)
